d15a54649ffd87215cae17f940e36a5a0b69a062
Masaki Ikeda <masaki.ikeda@zapatacomputing.com>
2021-04-16 00:03:47 +0900

    Update Exercise_2.2.py

--- Exercise_2.2.py
+++ Exercise_2.2.py
@@ -4,12 +4,11 @@ This function saves a welcome message.
 
 import json
 
-def welcome():
-    message = "Welcome to Orquestra! Nice to meet you, "
+def yourName(name):
 
     message_dict = {}
-    message_dict["message"] = message
-    message_dict["schema"] = "message"
+    message_dict["name"] = name
+    message_dict["schema"] = "name"
 
-    with open("welcome.json",'w') as f:
+    with open("yourName.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
